b312694e665953aac6a8168ff1c6c47bc349ba27
Camille-Esp <camille.espieux@gmail.com>
2025-11-10 09:22:00 +0100

    Exercice 2.2 fini

--- Full_DevOps_Lab/Full_DevOps_Lab/src/js-practice.js
+++ Full_DevOps_Lab/Full_DevOps_Lab/src/js-practice.js
@@ -9,3 +9,30 @@ if (open) {
 else {
     console.log("Sorry, we are closed now.");
 }
+
+//2.2 Working with Arrays and Loops
+let doctors = ["Lee", "Khan","Smith","Patel","Garcia"];
+
+function listDoctors(docArray) {
+    let string = "";
+    for (let i=0; i < doctors.length; i++) {
+        string += "Doctor #" + (i+1) +": Dr. " + doctors[i] +"\n";
+    }
+    return string;
+}
+
+console.log(listDoctors(doctors));
+doctors.push("Johnson");
+console.log(listDoctors(doctors));
+
+function findDoctor(docArray, name) {
+    if(docArray.includes(name)) {
+        return "Doctor found";
+    } else {
+        return "Doctor not found";
+    }
+}
+
+console.log("\nDoes Dr. Smith work here ? " + findDoctor(doctors, "Smith"));
+console.log("Does Dr. Brown work here ? " + findDoctor(doctors, "Brown"));
+
