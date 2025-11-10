4acdaaa2d6a5de82d64df47eef1221a5164dfc1d
Camille-Esp <camille.espieux@gmail.com>
2025-11-10 09:13:59 +0100

    Exercice 2.1 fini

---
+++ Full_DevOps_Lab/Full_DevOps_Lab/src/js-practice.js
@@ -0,0 +1,11 @@
+// 2.1 Variables and Data Types
+let hospitalName = "Central Hospital";
+let nbrDoctors = 12;
+let open = true;
+
+if (open) {
+    console.log("Welcome to " + hospitalName + "! We have " + nbrDoctors + " doctors currently available.");
+}
+else {
+    console.log("Sorry, we are closed now.");
+}
