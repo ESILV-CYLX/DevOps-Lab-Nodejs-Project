6d0e3060fe6722b7b485fc95eb1bb8f08e88a015
Camille-Esp <camille.espieux@gmail.com>
2025-11-18 18:10:33 +0100

    Fin implementation classes

--- Full_DevOps_Lab/Full_DevOps_Lab/src/models/ShoppingList.js
+++ Full_DevOps_Lab/Full_DevOps_Lab/src/models/ShoppingList.js
@@ -2,12 +2,14 @@ export class ShoppingList {
   /**
    * @param {number} shoppingListId
    * @param {number} userId
+   * @param {Date} updatedAt
    * @param {Date} dateCreated
    */
-  constructor(shoppingListId, userId, dateCreated) {
+  constructor(shoppingListId, userId, dateCreated, updatedAt = new Date()) {
     this.shoppingListId = shoppingListId;
     this.userId = userId;
     this.dateCreated = dateCreated;
+    this.updatedAt = updatedAt;
     this.items = [];
   }
 
