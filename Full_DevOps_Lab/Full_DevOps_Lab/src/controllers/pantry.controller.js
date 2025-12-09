97bdc4978dc4eb022f557abbb2831fba5abe4ce8
DHXuan <brainless78.pyke@gmail.com>
2025-12-09 15:40:52 +0100

    pantry.controller.js mangoose update

--- Full_DevOps_Lab/Full_DevOps_Lab/src/controllers/pantry.controller.js
+++ Full_DevOps_Lab/Full_DevOps_Lab/src/controllers/pantry.controller.js
@@ -1,39 +1,38 @@
 import { getPantry, addToPantry, deleteFromPantry } from "../services/pantry.service.js";
 
-export function getPantryController(req, res) {
-    return res.json(getPantry());
+export async function getPantryController(req, res) {
+    const pantryItems = await getPantry();
+    res.json(pantryItems);
 }
 
-export function addPantryController(req, res) {
+export async function addPantryController(req, res) {
     const { nid, nquantity } = req.body;
 
     if (!nid || !nquantity)
         return res.status(400).json({ error: "Données incomplètes" });
 
     try {
-        const result = addToPantry(nid, nquantity);
+        const result = await addToPantry(nid, nquantity);
         return res.status(201).json(result);
     } catch (err) {
         if (err.message === "INVALID_QUANTITY")
             return res.status(400).json({ error: "Quantité prohibée" });
-
         return res.status(500).json({ error: "Erreur interne" });
     }
 }
 
-export function deletePantryController(req, res) {
+export async function deletePantryController(req, res) {
     const { nid } = req.body;
 
     if (!nid)
         return res.status(400).json({ error: "Données incomplètes" });
 
     try {
-        deleteFromPantry(nid);
+        await deleteFromPantry(nid);
         return res.json({ message: "Ingrédient supprimé" });
     } catch (err) {
         if (err.message === "NOT_FOUND")
             return res.status(404).json({ error: "Ingrédient introuvable" });
-
         return res.status(500).json({ error: "Erreur interne" });
     }
 }
