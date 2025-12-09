c080e32e48a1c25efe5379f5dd9ba8f3008e6542
Camille-Esp <camille.espieux@gmail.com>
2025-12-09 16:19:25 +0100

    Modification models : correction lien entre eux et utilisation incorrecte des énumérations

--- Full_DevOps_Lab/Full_DevOps_Lab/src/models/Ingredient.js
+++ Full_DevOps_Lab/Full_DevOps_Lab/src/models/Ingredient.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose";
+import { IngredientCategory } from "./enums/IngredientCategory.js";
 
 //Ingredient Schema for embedding
 const ingredientSchema = new mongoose.Schema({
     ingredientId: { type: Number, required: true, unique: true},
     name: { type: String, required: true},
-    category: { type: String, required: true},
+    category: { 
+        type: String, 
+        enum: Object.values(IngredientCategory), 
+        required: true 
+    },
     price: { type: Number, required: true}
 });
 
